d6db4bf323c328f18902548542e7ec79093a9474
Jayaram <jay@ks.com>
2017-01-13 12:13:44 +0400

    added

--- routes/manga.php
+++ routes/manga.php
@@ -2,7 +2,5 @@
 edited from MASTER branch
 BRANCH 2 editing id done here 2
 BRANCH 2 editing id done here2
-BRANCH 2 editing id done here3
-
-to push to remote repository
+BRANCH 2 editing id done
 
